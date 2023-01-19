f4b178d8ac8a13238e72a03d42aab356e2aeaef9
Hyebin-Woo <dev.hyebin@gmail.com>
2023-01-20 00:22:18 +0900

    :lipstick: 결과페이지

--- js/start.js
+++ js/start.js
@@ -4,6 +4,31 @@ const result = document.querySelector("#result");
 let q = document.querySelector(".qBox");
 let a = document.querySelector(".answerBox");
 const endPoint = 12;
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+function calResult() {
+    let result = select.indexOf(Math.max(...select));
+    return result;
+}
+
+function setResult() {
+    let point = calResult();
+    const resultName = document.querySelector(".resultname");
+    resultName.innerHTML = infoList[point].name;
+
+    // 결과 이미지 뿌려주기
+    let resultImg = document.createElement("img");
+    const imgDiv = document.querySelector("#resultImg");
+    let imgURL = "img/image-" + point + ".png";
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add("img-fluid");
+    imgDiv.appendChild(resultImg);
+
+    // 결과 설명 뿌려주기
+    const resultDesc = document.querySelector(".resultDesc");
+    resultDesc.innerHTML = infoList[point].desc;
+}
 
 //결과 페이지
 function goResult() {
@@ -16,26 +41,30 @@ function goResult() {
             qna.style.display = "none";
             result.style.display = "block";
         }, 450);
-        let qIdx = 0;
-        goNext(qIdx);
-    }, 450);
+    });
+    setResult();
 }
 
-function addAnswer(answerText, qIdx) {
+function addAnswer(answerText, qIdx, idx) {
     let answer = document.createElement("button");
     answer.classList.add("answerList");
     a.appendChild(answer);
     answer.innerHTML = answerText;
     answer.classList.add(".fadeIn");
 
-    answer.addEventListener("click", () => {
-        let children = document.querySelectorAll(".answerList");
+    answer.addEventListener("click", function () {
+        var children = document.querySelectorAll(".answerList");
         for (let i = 0; i < children.length; i++) {
             children[i].disabled = true;
-            children[i].style.WebkitAnimation = "fadeOut 0.7s";
-            children[i].style.animation = "fadeOut 0.7s";
+            children[i].style.WebkitAnimation = "fadeOut 0.5s";
+            children[i].style.animation = "fadeOut 0.5s";
         }
         setTimeout(() => {
+            var target = qnaList[qIdx].a[idx].type;
+            for (let i = 0; i < target.length; i++) {
+                select[target[i]] += 1;
+            }
+
             for (let i = 0; i < children.length; i++) {
                 children[i].style.display = "none";
             }
@@ -45,14 +74,14 @@ function addAnswer(answerText, qIdx) {
 }
 
 function goNext(qIdx) {
-    if (qIdx + 1 === endPoint) {
+    if (qIdx === endPoint) {
         goResult();
         return;
     }
 
     q.innerHTML = qnaList[qIdx].q;
     for (let i in qnaList[qIdx].a) {
-        addAnswer(qnaList[qIdx].a[i].answer, qIdx);
+        addAnswer(qnaList[qIdx].a[i].answer, qIdx, i);
     }
     // 진행상태바 구현하기
     let status = document.querySelector(".statusBar");
